20c9496b9aa0cf32b69ca00f6d432dff817f1017
shekarbadachi <48435166+shekarbadachi@users.noreply.github.com>
2019-03-12 16:02:44 +0530

    updated list contents

--- my_prog.py
+++ my_prog.py
@@ -1,20 +1,36 @@
-class lst_com():
+import re
+
+
+class LstCom:
     def __init__(self, lst1, lst2):
         self.lst1 = lst1
         self.lst2 = lst2
-        flag = 0
-        i = 0
-        for s1 in self.lst1:
-                if s1 != self.lst2[i]:
-                    flag = 1
-                i += 1
-        if flag is 0:
-            print("lists are equal")
-        else:
-            print("unequal")
+
+    def cmp(self):
+        print "from first list"
+        for ele in self.lst1:
+            e = re.sub("ReportRequest", "", ele)
+            print e
+        print "\n ==============================================="
+        for ele1 in self.lst2:
+            e1 = re.sub("_Report", "", ele1)
+            print e1
 
 
 if __name__ == "__main__":
-    l1 = ["a", "b", "c"]
-    l2 = ["a", "b", "c"]
-    lst = lst_com(l1, l2)
+    l1 = ['AdDynamicTextPerformanceReportRequest', 'AdExtensionByAdReportRequest', 'AdExtensionByKeywordReportRequest',
+          'AdExtensionDetailReportRequest', 'AdGroupPerformanceReportRequest', 'AdPerformanceReportRequest',
+          'AgeGenderDemographicReportRequest', 'AudiencePerformanceReportRequest', 'BudgetSummaryReportRequest',
+          'CallDetailReportRequest', 'CampaignPerformanceReportRequest', 'ConversionPerformanceReportRequest',
+          'DestinationUrlPerformanceReportRequest', 'GeographicPerformanceReportRequest',
+          'GoalsAndFunnelsReportRequest', 'KeywordPerformanceReportRequest', 'NegativeKeywordConflictReportRequest',
+          'ProductDimensionPerformanceReportRequest', 'SearchCampaignChangeHistoryReportRequest',
+          'SearchQueryPerformanceReportRequest', 'ShareOfVoiceReportRequest', 'UserLocationPerformanceReportRequest']
+
+    l2 = ['AdExtensionByAd_Report', 'AdExtensionByKeyword_Report', 'AdExtensionDetail_Report',
+          'AgeGenderDemographic_Report', 'AudiencePerformance_Report', 'BudgetSummary_Report',
+          'CallDetail_Report', 'ConversionPerformance_Report', 'GeographicPerformance_Report',
+          'GoalsAndFunnels_Report', 'PublisherUsagePerformance_Report', 'SearchCampaignChangeHistory_Report',
+          'SearchQueryPerformance_Report', 'ShareOfVoice_Report', 'UserLocationPerformance_Report']
+    lst = LstCom(l1, l2)
+    lst.cmp()

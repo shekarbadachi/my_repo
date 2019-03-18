9c842ef4416dfcccb162cf93ffecae4bab66753c
shekarbadachi <48435166+shekarbadachi@users.noreply.github.com>
2019-03-18 17:00:01 +0530

    Update my_prog.py

--- my_prog.py
+++ my_prog.py
@@ -1,36 +1,75 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""list manipulation"""
 import re
 
 
-class LstCom:
+class LstCom(object):
+    """extracts word from list"""
+
     def __init__(self, lst1, lst2):
         self.lst1 = lst1
         self.lst2 = lst2
 
     def cmp(self):
-        print "from the first list"
+        """compares lists"""
+        print 'from first list'
         for ele in self.lst1:
-            e = re.sub("ReportRequest", "", ele)
+            e = re.sub('ReportRequest', '', ele)
             print e
-        print "\n ==============================================="
+        print '\n ==============================================='
         for ele1 in self.lst2:
-            e1 = re.sub("_Report", "", ele1)
+            e1 = re.sub('_Report', '', ele1)
             print e1
 
 
-if __name__ == "__main__":
-    list1 = ['AdDynamicTextPerformanceReportRequest', 'AdExtensionByAdReportRequest', 'AdExtensionByKeywordReportRequest',
-          'AdExtensionDetailReportRequest', 'AdGroupPerformanceReportRequest', 'AdPerformanceReportRequest',
-          'AgeGenderDemographicReportRequest', 'AudiencePerformanceReportRequest', 'BudgetSummaryReportRequest',
-          'CallDetailReportRequest', 'CampaignPerformanceReportRequest', 'ConversionPerformanceReportRequest',
-          'DestinationUrlPerformanceReportRequest', 'GeographicPerformanceReportRequest',
-          'GoalsAndFunnelsReportRequest', 'KeywordPerformanceReportRequest', 'NegativeKeywordConflictReportRequest',
-          'ProductDimensionPerformanceReportRequest', 'SearchCampaignChangeHistoryReportRequest',
-          'SearchQueryPerformanceReportRequest', 'ShareOfVoiceReportRequest', 'UserLocationPerformanceReportRequest']
+def main():
+    """main function"""
+    lst1 = [
+        'AdDynamicTextPerformanceReportRequest',
+        'AdExtensionByAdReportRequest',
+        'AdExtensionByKeywordReportRequest',
+        'AdExtensionDetailReportRequest',
+        'AdGroupPerformanceReportRequest',
+        'AdPerformanceReportRequest',
+        'AgeGenderDemographicReportRequest',
+        'AudiencePerformanceReportRequest',
+        'BudgetSummaryReportRequest',
+        'CallDetailReportRequest',
+        'CampaignPerformanceReportRequest',
+        'ConversionPerformanceReportRequest',
+        'DestinationUrlPerformanceReportRequest',
+        'GeographicPerformanceReportRequest',
+        'GoalsAndFunnelsReportRequest',
+        'KeywordPerformanceReportRequest',
+        'NegativeKeywordConflictReportRequest',
+        'ProductDimensionPerformanceReportRequest',
+        'SearchCampaignChangeHistoryReportRequest',
+        'SearchQueryPerformanceReportRequest',
+        'ShareOfVoiceReportRequest',
+        'UserLocationPerformanceReportRequest',
+    ]
 
-    list2 = ['AdExtensionByAd_Report', 'AdExtensionByKeyword_Report', 'AdExtensionDetail_Report',
-          'AgeGenderDemographic_Report', 'AudiencePerformance_Report', 'BudgetSummary_Report',
-          'CallDetail_Report', 'ConversionPerformance_Report', 'GeographicPerformance_Report',
-          'GoalsAndFunnels_Report', 'PublisherUsagePerformance_Report', 'SearchCampaignChangeHistory_Report',
-          'SearchQueryPerformance_Report', 'ShareOfVoice_Report', 'UserLocationPerformance_Report']
-    lst = LstCom(list1, list2)
-    lst.cmp()
+    lst2 = [
+        'AdExtensionByAd_Report',
+        'AdExtensionByKeyword_Report',
+        'AdExtensionDetail_Report',
+        'AgeGenderDemographic_Report',
+        'AudiencePerformance_Report',
+        'BudgetSummary_Report',
+        'CallDetail_Report',
+        'ConversionPerformance_Report',
+        'GeographicPerformance_Report',
+        'GoalsAndFunnels_Report',
+        'PublisherUsagePerformance_Report',
+        'SearchCampaignChangeHistory_Report',
+        'SearchQueryPerformance_Report',
+        'ShareOfVoice_Report',
+        'UserLocationPerformance_Report',
+    ]
+    lst_compare = LstCom(lst1, lst2)
+    lst_compare.cmp()
+
+
+if __name__ == '__main__':
+    main()
